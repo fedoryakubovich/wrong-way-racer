23627e647a9572d71c73963412329fa2f6a88350
Fedor Yakubovich <f.yakubovich@gmail.com>
2023-01-09 02:10:30 +0300

    Update Players module

---
+++ src/modules/Players/containers/PlayersContainer.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { APP_ACTIONS, useAppState } from "../../../store";
+import { socket, SOCKET_EVENTS } from "../../../utils";
+
+import Players from "../components/Players";
+
+const PlayersContainer = () => {
+  const [players, setPlayers] = useState([]);
+  const { dispatch } = useAppState();
+
+  const handleClickOpen = () => {
+    dispatch({ type: APP_ACTIONS.openModal });
+  };
+
+  useEffect(() => {
+    socket.on(SOCKET_EVENTS.players, (data) => {
+      setPlayers(data);
+    });
+  }, []);
+
+  return <Players players={players} handleClickOpen={handleClickOpen} />;
+};
+
+export default PlayersContainer;
